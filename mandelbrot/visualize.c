a2a069e8c3e47e8463e9ff6164332f04dd6413f5
LikiHamakiotes <113614399+LikiHamakiotes@users.noreply.github.com>
2022-10-11 15:04:44 -0400

    Update visualize.c

--- mandelbrot/visualize.c
+++ mandelbrot/visualize.c
@@ -51,7 +51,65 @@ It should look something like this:
 
                                                                 *
 */
+#include <assert.h>
 #include <stdio.h> /* printf */
-#include "mandelbrot.h" /* InMandelbrotSet */
 
-int main() {}
+int inMandelbrotSet (double a, double b) {
+    double real = 0;
+    double imaginary = 0;
+
+    for(int x = 0; x < 100; x++) {
+      double a_ = real*real - imaginary*imaginary;
+      double b_ = 2*real*imaginary;
+
+      a_ = a_ + a;
+      b_ = b_ + b;
+
+      real = a_;
+      imaginary = b_;
+      
+      /* the real parts combining with the imaginary parts */
+
+      if(real < 0) {
+        if(real*(-1) > 2) {
+          return 0;
+        }
+      }
+      else {
+        if(real < 0) {
+          return 0;
+      
+        }
+      }
+
+      if(imaginary < 0){
+        if(imaginary*(-1) > 2){
+          return 0;
+        }
+      }
+    }
+    return 1;
+  }
+
+/* absolute values being greater than 2 */
+
+int main() {
+  for(int m = -16; m < 16; m++) {
+    printf("\n");
+    for(int n = -64; n < 16; n++) {
+      double x = 0.03125*n;
+      double y = 0.0625*m;
+
+/* points that it needs to plot and their range */
+
+      if(inMandelbrotSet(x,y)) {
+        printf("%c", '*');
+      }
+      else {
+        printf("%c", ' ');
+      }
+    }
+  }
+  return 0;
+}
+ /* printing '*' if it is in the mandelbrot set and ' ' if it is not */
