ab4caaa00d43b61a7bd7bc084e7f36f4e4742673
LikiHamakiotes <113614399+LikiHamakiotes@users.noreply.github.com>
2022-10-11 14:48:17 -0400

    Update mandelbrot.c

--- mandelbrot/mandelbrot.c
+++ mandelbrot/mandelbrot.c
@@ -19,4 +19,77 @@ so if that is ever the case, we return 0.  Otherwise, if we do 100 iterations
 and have not yet returned, we return 1.
 */
 
-int inMandelbrotSet(double a, double b) {}
+#include <assert.h>
+
+int inMandelbrotSet (double a, double b) {
+    double real = 0;
+    double imaginary = 0;
+
+    for(int x = 0; x < 100; x++) {
+      double a_ = real*real - imaginary*imaginary;
+      double b_ = 2*real*imaginary;
+
+      a_ = a_ + a;
+      b_ = b_ + b;
+
+      real = a_;
+      imaginary = b_;
+
+      if(real < 0) {
+        if(real*(-1) > 2) {
+          return 0;
+        }
+      }
+      else {
+        if(real < 0) {
+          return 0;
+      
+        }
+      }
+
+      if(imaginary < 0){
+        if(imaginary*(-1) > 2){
+          return 0;
+        }
+      }
+    }
+    return 1;
+  }
+
+int main() {
+    assert(inMandelbrotSet(-0.90625, 1) == 0);
+    assert(inMandelbrotSet(-0.875, 0.9375) == 0);
+    assert(inMandelbrotSet(-0.84375, 0.875) == 0);
+    assert(inMandelbrotSet(-0.8125, 0.8125) == 0);
+    assert(inMandelbrotSet(-0.78125, 0.75) == 0);
+    assert(inMandelbrotSet(-0.75, 0.6875) == 0);
+    assert(inMandelbrotSet(-0.71875, 0.625) == 0);
+    assert(inMandelbrotSet(-0.6875, 0.5625) == 0);
+    assert(inMandelbrotSet(-0.65625, 0.5) == 0);
+    assert(inMandelbrotSet(-0.625, 0.4375) == 1);
+    assert(inMandelbrotSet(-0.59375, 0.375) == 1);
+    assert(inMandelbrotSet(-0.5625, 0.3125) == 1);
+    assert(inMandelbrotSet(-0.53125, 0.25) == 1);
+    assert(inMandelbrotSet(-0.5, 0.1875) == 1);
+    assert(inMandelbrotSet(-0.46875, 0.125) == 1);
+    assert(inMandelbrotSet(-0.4375, 0.0625) == 1);
+    assert(inMandelbrotSet(-0.40625, 0) == 1);
+    assert(inMandelbrotSet(-0.375, -0.0625) == 1);
+    assert(inMandelbrotSet(-0.34375, -0.125) == 1);
+    assert(inMandelbrotSet(-0.3125, -0.1875) == 1);
+    assert(inMandelbrotSet(-0.28125, -0.25) == 1);
+    assert(inMandelbrotSet(-0.25, -0.3125) == 1);
+    assert(inMandelbrotSet(-0.21875, -0.375) == 1);
+    assert(inMandelbrotSet(-0.1875, -0.4375) == 1);
+    assert(inMandelbrotSet(-0.15625, -0.5) == 1);
+    assert(inMandelbrotSet(-0.125, -0.5625) == 1);
+    assert(inMandelbrotSet(-0.09375, -0.625) == 1);
+    assert(inMandelbrotSet(-0.0625, -0.6875) == 1);
+    assert(inMandelbrotSet(-0.03125, -0.75) == 1);
+    assert(inMandelbrotSet(0, -0.8125) == 0);
+    assert(inMandelbrotSet(0.03125, -0.875) == 0);
+    assert(inMandelbrotSet(0.0625, -0.9375) == 0);
+    assert(inMandelbrotSet(0.09375, -1) == 0);
+
+    return 0;
+} 
